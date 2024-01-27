a895ab104a19075103415be81065852fb094de1d
Lauren <43707274+Lorn12@users.noreply.github.com>
2024-01-26 18:00:10 -0600

    Imported bootstrap

--- src/App.js
+++ src/App.js
@@ -4,17 +4,19 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <footer>
-        Coded By: Lauren Wooten and is open sourced &nbsp;
-        <br />
-        <a
-          href="https://github.com/Lorn12/days-of-weather-shecodes"
-          target="_blank"
-          rel="noreferrer"
-        >
-          D A Y S &nbsp; O F &nbsp; W E A T H E R
-        </a>
-      </footer>
+      <div className="container">
+        <footer>
+          Coded By: Lauren Wooten and is open sourced &nbsp;
+          <br />
+          <a
+            href="https://github.com/Lorn12/days-of-weather-shecodes"
+            target="_blank"
+            rel="noreferrer"
+          >
+            D A Y S &nbsp; O F &nbsp; W E A T H E R
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
